56788a072e934190a3e16f38224cb52004f95fa0
Jordi Alejandro García Núñez <jordign.mx@hotmail.com>
2015-02-12 10:47:37 -0600

    index changes
    se creo el controlador de prueba.php y se cambio en la ruta el acceso a
    ese controlador,, en el controlador se creo la clase Prueba y se hace
    referencia al nuevo archivo inicio.php

---
+++ application/views/inicio.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php 
+		echo "Hola mundo <br>";
+		echo "Hoy es: ".date("d-m-y");
+		echo "Bienvenido: ".$nombre
+	 ?>
+</body>
+</html>
