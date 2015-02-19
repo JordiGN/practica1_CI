0c6ebfabc7fee7f18cda3d43f09fcbb1a078a220
Jordi Alejandro García Núñez <jordign.mx@hotmail.com>
2015-02-19 07:31:28 -0600

    Secciones en CodeIgniter
    Se hicieron las carpetas de:
    Cabecera,menu y pie, donde se repartieron las lineas de codigo del
    index.php y aun asi se pudo visualizar completamente el contenido de la
    pagina, para cargar las partes de codigo separadas se utilzia el comando
    "Include" seguido de la direccion de la carpeta separado por /,
    ovbamente habiendola relacionada con la url Base

--- application/views/inicio.php
+++ application/views/inicio.php
@@ -1,24 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-	<base href="<?php echo base_url(); ?>"> 
-	<meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
     <?php 
+    include "/secciones/cabecera.php";
+
+
 		echo "<h1>Hola mundo</h1> <br>";
 		echo "Hoy es: ".date("d-m-y");
 		echo "<h2>Bienvenido: </h2>".$nombre;
@@ -30,12 +14,7 @@
 	<a href="index.php/prueba/opcion3">opcion 3</a>
 	<a href="http://www.itcolima.edu.mx">Tec De Colima xD</a>
 	<?php 
-	 ?>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
+  include "/secciones/pie.php";
+	 ?>
 
--- application/views/vista2.php
+++ application/views/vista2.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<base href="<?php echo base_url(); ?>"> 
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
-	<?php 
+    <?php 
+    include "/secciones/cabecera.php";
 		echo "<h1>Vista2</h1> <br>";
 		echo "Hoy es: ".date("d-m-y");
 		echo "<h2>Bienvenido: </h2>";
@@ -14,7 +7,8 @@
 	<a href="index.php/prueba/opcion1">opcion 1</a>
 	<a href="index.php/prueba/opcion2">opcion 2</a>
 	<a href="index.php/prueba/opcion3">opcion 3</a>
+	<a href="index.php">Volver</a>
 	<?php 
+
+  include "/secciones/pie.php";
 	 ?>
-</body>
-</html>
